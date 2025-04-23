6a757ca4291fe15f68fada067a476a9afccb5728
kkithome <117781574+kkithome@users.noreply.github.com>
2025-04-23 15:06:51 -0400

    Author: Kaluki; Estimated Time: 25 minutes (including research); Content: Sample Tkinter Code

---
+++ example.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import Tk
+from tkinter import *
+
+root=Tk() # creates graphical window
+a = Label(root, text="Hello, world! And welcome to the main event!")
+a.pack()
+root.mainloop()
+
+
+
